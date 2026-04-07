afa8faaf7a118459d1eee23ad98e80be09c3efb6
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2026-04-07 23:53:14 +0530

    feat: Add Landing Page and Recruiter Dashboard
    - Updated package.json to include new dependencies: axios, react-router, react-router-dom, and react-scroll.
    - Implemented routing in App.jsx with a Root component for navigation.
    - Created LandingPage component with sections for home, about, features, and contact, including a navigation bar and footer.
    - Added Recruiter_Dashboard component as a placeholder for future development.
    - Included images for the LandingPage.

--- Frontend/skillsync/src/App.jsx
+++ Frontend/skillsync/src/App.jsx
@@ -1,10 +1,38 @@
+import {createBrowserRouter , RouterProvider } from 'react-router-dom'
+import { Navigate } from 'react-router-dom';
+
+//Pages
+import Recuiter_Dashboard from '../src/component/Pages/recuiter/Recuiter_Dashboard.jsx'
+import LandingPage from './component/LandingPage.jsx';
+
+
+const Root = () => {
+  const token = localStorage.getItem("token");
+
+  return token 
+    ? <Navigate to="/recruiter" replace />
+    : <Navigate to="/landing-page" replace />;
+};
+
+const route = createBrowserRouter([
+  {
+    path:'/',
+    element:<Root/>
+  },
+  {
+    path:'/landing-page',
+    element:<LandingPage/>
+  },
+  {
+    path:'/recruiter',
+    element:<Recuiter_Dashboard/>
+  }
+])
 
 function App() {
   
   return (
-    <>
-    app
-    </>
+    <RouterProvider router={route}/>
   )
 }
 
---
+++ Frontend/skillsync/src/component/Pages/recuiter/Recuiter_Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Recuiter_Dashboard() {
+  return (
+    <div>Recuiter_Dashboard</div>
+  )
+}
+
+export default Recuiter_Dashboard
